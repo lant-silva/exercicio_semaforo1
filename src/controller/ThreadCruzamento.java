65dcb5c25f2a2031bb85e2dfb69f55689b8d7f1d
lant-silva <luiz.antoniocruz2008@hotmail.com>
2023-03-14 23:48:45 -0300

    Commit final

---
+++ src/controller/ThreadCruzamento.java
@@ -0,0 +1,63 @@
+package controller;
+import java.util.concurrent.Semaphore;
+
+public class ThreadCruzamento extends Thread {
+
+	int idcarro;
+	Semaphore semaforo;
+	static String direção;
+	static int aguardar = 100;
+	
+	public ThreadCruzamento(int i, Semaphore semaforo) {
+		this.semaforo = semaforo;
+		idcarro = i;
+	}
+	
+	@Override
+	public void run() {
+		carroDireção();
+		
+		try {
+			semaforo.acquire();
+			entrarCruzamento();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+
+		}
+	}
+	
+	public void carroDireção() {
+		int sentido = (int) (Math.random()*4)+1;
+		switch (sentido) {
+		case 1: direção = "Norte";
+		break;
+		case 2: direção = "Sul";
+		break;
+		case 3: direção = "Leste";
+		break;
+		case 4: direção = "Oeste";
+		break;
+		}
+		
+	}
+	
+	public void entrarCruzamento() {
+		
+		System.out.println("O carro "+idcarro+" entrou no cruzamento");
+		System.out.println("Direcao "+direção);
+		sairCruzamento();
+	}
+	
+	public void sairCruzamento() {
+		int tempoEspera = (int) (Math.random()*2001);
+		try {
+			Thread.sleep(tempoEspera);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Carro "+idcarro+" saiu do cruzamento");
+	}
+	
+}
---
+++ src/view/Main.java
@@ -0,0 +1,20 @@
+package view;
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadCruzamento;
+
+public class Main {
+
+	public static Semaphore semaforo;
+		
+	public static void main(String[] args) {
+		int carros=10;
+		semaforo = new Semaphore(1);
+		
+		for(int i=1;i<=carros;i++) {
+			Thread cruzamento = new ThreadCruzamento(i, semaforo);
+			cruzamento.start();
+		}
+		
+	}
+}
